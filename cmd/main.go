b6bed6c060ec11d2dd0e9c35e2821a0dd85e099a
Oleksiy Pyltsov <valar999@gmail.com>
2023-01-26 13:19:44 +0100

    Debug

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,6 @@ import (
 )
 
 func main() {
-        a := "aaaaaaaaaa"
-        b := "aaaaaaaaaa"
-        c := "aaaaaaaaaa"
-        fmt.Println(a, b, c)
 	// Basic command-line flag parsing
 	exampleInt := flag.Int("n", 42, "help message for flag n")
 	exampleStr := flag.String("example", "foo", "help message")
